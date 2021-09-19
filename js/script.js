10f9e525caf4dd80aa178efe068adb162dc6f870
Amol4641 <gosaviamol94@gmail.com>
2021-09-19 11:00:56 +0530

    third iteration

--- js/script.js
+++ js/script.js
@@ -36,7 +36,7 @@ function popOut(){
 
 function startGame(){
     countdown = timeLimit/1000;
-    scoreBoard.textContent = 0;
+    scoreBoard.textContent ='SCORE :'+ 0;
     scoreBoard.style.display = 'block';
     countdownBoard.textContent = countdown;
     timeUp = false;
@@ -60,6 +60,7 @@ function startGame(){
 startButton.addEventListener('click', startGame);
 
 function catchMe(e){
+    document.getElementById("audio").play();
     score++;
     this.style.backgroundImage = 'url("https://images.unsplash.com/photo-1585129918930-d1f1179a81e2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYzMTk3MTAwMg&ixlib=rb-1.2.1&q=80&w=400")';
     this.style.pointerEvents = 'none';
@@ -68,7 +69,7 @@ function catchMe(e){
         this.style.pointerEvents = 'all';
 
     }, 1200);
-    scoreBoard.textContent ="Score " + score;
+    scoreBoard.textContent ="SCORE :" + score;
 
 }
 // let x = document.getElementById("set");
